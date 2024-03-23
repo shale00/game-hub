bd29821f9db1c3a8a05892a799e331bbb56eef96
sth <sth0067@gmail.com>
2024-03-23 00:51:44 -0500

    Created a custom hook for fetching games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,33 +1,18 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    axios
-      .get<FetchGamesResponse>("http://localhost:3001/api/data")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err));
-  }, []);
+  const { games, error } = useGames();
 
   return (
-    <ul>
-      {games.map((game) => (
-        <li key={game.id}>{game.name}</li>
-      ))}
-    </ul>
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,37 @@
+import axios, { CanceledError } from "axios";
+import { useState, useEffect } from "react";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    axios
+      .get<FetchGamesResponse>("http://localhost:3001/api/data", {
+        signal: controller.signal,
+      })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
