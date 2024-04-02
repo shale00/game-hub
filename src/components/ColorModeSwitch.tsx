9314ed354df3d8737fbcd819770b32e809f03a44
sth <sth0067@gmail.com>
2024-04-02 16:03:42 -0500

    Built search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -9,7 +9,7 @@ const ColorModeSwitch = () => {
         onChange={toggleColorMode}
         isChecked={colorMode === "dark"}
       />
-      <Text>{colorMode === "dark" ? "Light" : "Dark"}</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
